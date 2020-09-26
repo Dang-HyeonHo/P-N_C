6edf200c2b8e2f242bb4940722e6e80a192049b1
Hyeonho <60058645+Dang-HyeonHo@users.noreply.github.com>
2020-09-27 00:15:15 +0900

    Multiplication table

---
+++ 6 Days - 20.09.26/[BAEKJOON_No.2739] 구구단.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// 문제
+// N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
+
+// 입력
+// 첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
+
+// 출력
+// 출력형식과 같게 N*1부터 N*9까지 출력한다.
+
+main()
+{
+	int input;
+
+	scanf_s("%d", &input);
+
+	if (1 <= input && input <= 9)
+	{
+		for (int num = 1; num <= 9; num++)
+		{
+			int result = input * num;
+			printf("%d * %d = %d\n", input, num, result);
+		}
+	}
+
+	return 0;
+}
